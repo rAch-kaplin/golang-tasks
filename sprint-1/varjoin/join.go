4380e8b497fc65aaddc8ed8dd14bf9ca9837ce36
Kaplin Artyom <kaplinstu1@gmail.com>
2025-06-21 17:26:03 +0300

    done varjoin

--- sprint-1/varjoin/join.go
+++ sprint-1/varjoin/join.go
@@ -2,6 +2,34 @@
 
 package varjoin
 
+import (
+		"strings"
+)
+
 func Join(sep string, args ...string) string {
-	return ""
+	if len(args) == 0 {
+		return ""
+	}
+
+	output := strings.Builder{}
+	output.Grow(TotalLen(sep, args...))
+
+	for i, s := range args {
+		if i > 0 {
+			output.WriteString(sep)
+		}
+		output.WriteString(s)
+	}
+
+	return output.String()
+}
+
+func TotalLen(sep string, args ...string) int {
+	sep_len := len(sep)
+	var total_len int
+	for _, s := range args {
+		total_len += len(s) + sep_len
+	}
+
+	return total_len - sep_len
 }
